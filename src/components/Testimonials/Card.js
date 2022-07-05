e1590595e6acbdf799fa1b9f5a6209a120ad0c1e
maclemire <pierrelemire@hotmail.fr>
2022-07-05 15:07:08 +0200

    Testimonials

--- src/components/Testimonials/Card.js
+++ src/components/Testimonials/Card.js
@@ -3,19 +3,16 @@ import React from 'react'
 export default function Card( {titre, texte, image, nom}) {
   return (
     <div>
-        <div className='bg-white mt-24'>
-            <p className='text-primary'>{titre}</p>
-            <p className='text-grey-dark'>{texte}</p>
-            <img className='' src={image} alt="" />
-            <p className='text-secondary'> {nom} <span className='text-grey-dark'>customer</span></p>
-        </div>
+      <div className="bg-white mt-24 pt-4 pb-12 px-8">
+        <p className="text-primary pb-6 font-extrabold">{titre}</p>
+        <p className="text-grey-dark text-xs leading-relaxed font-medium">{texte}</p>
+        <img className="rounded-full w-12 h-12 my-6" src={image} alt="" />
+        <p className="text-secondary">
+         {nom} <span className="text-grey-dark">customer</span>
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-<Card
-  titre=""
-  texte="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim, molestiae cumque nam recusandae quasi corrupti, non natus adipisci ducimus repellat consequuntur repudiandae odit a doloremque."
-  image=""
-  nom=""
-/>;
+
--- src/pages/Testimonials.js
+++ src/pages/Testimonials.js
@@ -7,7 +7,26 @@ export default function Testimonials() {
   return (
     <Layout>
       <Base phrase="Ce que les clients disent" titre="Témoignages" />
-      <Card />
+      <div className="grid grid-cols-3 gap-12">
+        <Card
+          titre="Rapide et efficace"
+          texte="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim, molestiae cumque nam recusandae quasi corrupti, non natus adipisci ducimus repellat consequuntur repudiandae odit a doloremque."
+          image="homme1.webp"
+          nom="Diane Smith"
+        />
+        <Card
+          titre="Fort de proposition"
+          texte="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim, molestiae cumque nam recusandae quasi corrupti, non natus adipisci ducimus repellat consequuntur repudiandae odit a doloremque."
+          image="homme2.webp"
+          nom="Diane smith"
+        />
+        <Card
+          titre="Agréable et professionel"
+          texte="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim, molestiae cumque nam recusandae quasi corrupti, non natus adipisci ducimus repellat consequuntur repudiandae odit a doloremque."
+          image="femme.webp"
+          nom="Diane Smith"
+        />
+      </div>
     </Layout>
   );
 }
